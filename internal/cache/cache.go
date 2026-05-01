ee3b71cf1cba27bc17f332d44d06a61deed4d2e5
safkhamezadeh <s.afkhamezadeh@gmail.com>
2026-05-01 12:39:23 +0200

    cache and ranker implemented

---
+++ internal/cache/cache.go
@@ -0,0 +1,73 @@
+package cache
+
+import (
+	"errors"
+	jobvacancies "job_vacancies/internal/job_vacancies"
+	"sync"
+	"time"
+)
+
+type InMemoryCache struct {
+	mu    sync.RWMutex
+	cache map[string]cacheItem //map of id and slice of jobs
+	ttl   time.Duration
+}
+
+func (c *InMemoryCache) StartCleanup(interval time.Duration) {
+	go func() {
+		for {
+			time.Sleep(interval)
+			c.cleanup()
+		}
+	}()
+}
+
+func (c *InMemoryCache) cleanup() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	now := time.Now()
+
+	for id, item := range c.cache {
+		if now.Sub(item.lastAccessed) > c.ttl {
+			delete(c.cache, id)
+		}
+	}
+
+}
+
+func (c *InMemoryCache) Set(id string, data []jobvacancies.Job) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	item := cacheItem{jobs: data, lastAccessed: time.Now()}
+	c.cache[id] = item
+
+}
+
+func (c *InMemoryCache) Get(id string) ([]jobvacancies.Job, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	item, ok := c.cache[id]
+	if !ok {
+		return nil, errors.New("cache id doesnt exist")
+	}
+	if !item.validateTTL(c.ttl) {
+		delete(c.cache, id)
+		return nil, errors.New("search expired")
+	}
+	item.lastAccessed = time.Now()
+	c.cache[id] = item
+	return c.cache[id].jobs, nil
+}
+
+type cacheItem struct {
+	jobs         []jobvacancies.Job
+	lastAccessed time.Time
+}
+
+func (i cacheItem) validateTTL(ttl time.Duration) bool {
+	if time.Since(i.lastAccessed) < ttl {
+		return true
+	}
+	return false
+}
--- internal/ranker/ranker.go
+++ internal/ranker/ranker.go
@@ -8,7 +8,7 @@ import (
 )
 
 type JobWithPoints struct {
-	job    *jobvacancies.Job
+	job    jobvacancies.Job
 	points int
 }
 
@@ -20,55 +20,52 @@ func (SimpleJobRanker) RankJobs(keywords keywordextractor.KeyWordFormat, jobs []
 	var jobsWithPoints []JobWithPoints
 
 	for i := range jobs {
-		jobsWithPoints = append(jobsWithPoints, assignPoints(keywords, &jobs[i]))
+		jobsWithPoints = append(jobsWithPoints, assignPoints(keywords, jobs[i]))
 	}
 
 	slices.SortFunc(jobsWithPoints, cmpJobPoints)
 	var jobsWithoutPoints []jobvacancies.Job
 	for i := 0; i < len(jobsWithPoints); i++ {
-		jobsWithoutPoints = append(jobsWithoutPoints, *jobsWithPoints[i].job)
+		jobsWithoutPoints = append(jobsWithoutPoints, jobsWithPoints[i].job)
 	}
 
 	return jobsWithoutPoints
 
 }
 
-func assignPoints(keywords keywordextractor.KeyWordFormat, job *jobvacancies.Job) JobWithPoints {
-	var currPoints int
-	maxPoints := getMaxPoints(keywords.JobTitles)
-	for i := 0; i < len(keywords.JobTitles); i++ {
-		if strings.Contains(job.Description, keywords.JobTitles[i]) {
-			currPoints = currPoints + (maxPoints - i)
-		}
-	}
-	maxPoints = getMaxPoints(keywords.Keywords)
-	for i := 0; i < len(keywords.Keywords); i++ {
-		if strings.Contains(job.Description, keywords.JobTitles[i]) {
-			currPoints = currPoints + (maxPoints - i)
-		}
-	}
-	return JobWithPoints{job: job, points: currPoints}
+func assignPoints(keywords keywordextractor.KeyWordFormat, job jobvacancies.Job) JobWithPoints {
+	title := strings.ToLower(job.Title)
+	desc := strings.ToLower(job.Description)
+
+	points := 0
+
+	points += scoreList(title, desc, keywords.JobTitles, 3)
+	points += scoreList(title, desc, keywords.Keywords, 1)
+
+	return JobWithPoints{job: job, points: points}
 }
 
-func getShortest[v any](sl1 []v, sl2 []v) int {
-	if len(sl1) < len(sl2) {
-		return len(sl1)
-	}
-	return len(sl2)
-}
+func scoreList(title, desc string, list []string, multiplier int) int {
+	points := 0
+	max := len(list)
 
-func getMaxPoints[v any](sl []v) int {
-	return len(sl) * 2
+	for i, k := range list {
+		kw := strings.ToLower(k)
+		weight := max - i
+
+		if strings.Contains(title, kw) {
+			points += multiplier * weight * 3
+		}
+		if strings.Contains(desc, kw) {
+			points += multiplier * weight
+		}
+	}
+
+	return points
 }
 
 func cmpJobPoints(a JobWithPoints, b JobWithPoints) int {
-	if a.points < b.points {
-		return -1
-	} else if a.points > b.points {
-		return 1
-	} else {
-		return 0
-	}
+	return b.points - a.points
 }
 
 //per job assign points to it
